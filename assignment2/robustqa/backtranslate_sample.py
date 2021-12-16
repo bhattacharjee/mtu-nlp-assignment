e705761cd0fab648ab4b5987b75486739c012b20
Rajbir Bhattacharjee <r.bhattacharjee@eclecticiq.com>
2021-12-16 00:55:39 +0000

    backtranslation sample

---
+++ assignment2/robustqa/backtranslate_sample.py
@@ -0,0 +1,44 @@
+# Taken from https://amitness.com/back-translation/
+
+from transformers import MarianMTModel, MarianTokenizer
+
+target_model_name = 'Helsinki-NLP/opus-mt-en-ROMANCE'
+target_tokenizer = MarianTokenizer.from_pretrained(target_model_name)
+target_model = MarianMTModel.from_pretrained(target_model_name)
+
+en_model_name = 'Helsinki-NLP/opus-mt-ROMANCE-en'
+en_tokenizer = MarianTokenizer.from_pretrained(en_model_name)
+en_model = MarianMTModel.from_pretrained(en_model_name)
+
+def translate(texts, model, tokenizer, language="fr"):
+    # Prepare the text data into appropriate format for the model
+    template = lambda text: f"{text}" if language == "en" else f">>{language}<< {text}"
+    src_texts = [template(text) for text in texts]
+
+    # Tokenize the texts
+    encoded = tokenizer.prepare_seq2seq_batch(src_texts, return_tensors='pt')
+    
+    # Generate translation using model
+    translated = model.generate(**encoded)
+
+    # Convert the generated tokens indices back into text
+    translated_texts = tokenizer.batch_decode(translated, skip_special_tokens=True)
+    
+    return translated_texts
+
+
+def back_translate(texts, source_lang="en", target_lang="fr"):
+    # Translate from source to target language
+    fr_texts = translate(texts, target_model, target_tokenizer, 
+                         language=target_lang)
+
+    # Translate from target language back to source language
+    back_translated_texts = translate(fr_texts, en_model, en_tokenizer, 
+                                      language=source_lang)
+    
+    return back_translated_texts
+
+en_texts = ['This is so cool', 'I hated the food', 'They were very helpful']
+
+aug_texts = back_translate(en_texts, source_lang="en", target_lang="es")
+print(aug_texts)
