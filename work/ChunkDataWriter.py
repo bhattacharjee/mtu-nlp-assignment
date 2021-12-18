0af0cb2a0dddf6ef6766e9b544917118d20018b3
Rajbir Bhattacharjee <r.bhattacharjee@eclecticiq.com>
2021-12-18 16:46:35 +0000

    work in progress

--- work/ChunkDataWriter.py
+++ work/ChunkDataWriter.py
@@ -4,15 +4,63 @@ import os
 import shutil
 import pathlib
 
+import threading
+
 class ChunkDataWriter():
     """Uses pickle, but splits into multiple files.
     behaves like an array and supports the append() method,
     and finalize method()"""
-    def __init__(self, directory:str):
+    def __init__(self, directory:str, chunk_size=16):
         self.directory = directory
+        self.chunk_size = chunk_size
+        self.lock = threading.RLock()
+
+        # the current slice of the array
+        self.current_slice = list()
+
+        # Length of all items
+        self.max_length = 0
+
+        # Where does the current slice start from
+        self.current_slice_start = -1
+
 
         if not os.path.exists(self.directory):
             pathlib.Path(self.directory).mkdir(parents=True, exist_ok=False)
 
+    def get_current_slice_max(self):
+        # Return the index of the last element that can
+        # be stored in the current slice
+        if self.current_slice_start == -1:
+            return -1
+        else:
+            return self.current_slice_start + self.chunk_size - 1
+
+    def get_current_slice_end(self):
+        if self.current_slice_start == -1:
+            return -1
+        else:
+            return self.current_slice_start + len(self.current_slice)
+
+    def get_current_slice_size(self):
+        return len(self.current_slice)
+
+    def get_current_slice_offsets(self):
+        return self.current_slice_start, \
+            self.get_current_slice_end(), \
+            self.get_current_slice_max()
+
+    def append(self, x):
+        with self.lock:
+            if self.current_slice_start == -1:
+                self.current_slice_start = 0
+
+            self.max_length += 1
+
+
+
+
+
+        pass
 
 
--- work/TestChunkDataWriter.py
+++ work/TestChunkDataWriter.py
@@ -3,21 +3,49 @@ import os
 import unittest
 import shutil
 import pathlib
+import glob
 
 from ChunkDataWriter import *
 
 class TestChunkDataWriter(unittest.TestCase):
+    thepath = "/tmp/1/2"
+    thepathparent = "/tmp/1"
+    chunk_size = 16
 
     # Test if the destination directory is being created
     def test_directory_creation(self):
-        thepath = "/tmp/1/2"
-        thepathparent = "/tmp/1"
+        thepath = TestChunkDataWriter.thepath
+        thepathparent = TestChunkDataWriter.thepathparent
         if os.path.exists(thepathparent):
             shutil.rmtree(thepathparent)
         c = ChunkDataWriter(directory=thepath)
         self.assertTrue(os.path.exists(thepath))
         self.assertTrue(os.path.isdir(thepath))
         shutil.rmtree(thepathparent)
+
+    def test_write_single_item(self):
+        if os.path.exists(TestChunkDataWriter.thepathparent):
+            shutil.rmtree(TestChunkDataWriter.thepathparent)
+        c = ChunkDataWriter(\
+            directory=TestChunkDataWriter.thepath,\
+            chunk_size=TestChunkDataWriter.chunk_size)
+        c.append(1)
+
+        # Test that a pickle file is created
+        file_list = glob.glob(f"{TestChunkDataWriter.thepath}/*.pickle")
+        self.assertEqual(len(file_list), 1, "No pickle file created")
+
+        # Test that a json file describing the item is created
+        file_list = glob.glob(f"{TestChunkDataWriter.thepath}/*.json")
+        self.assertEqual(len(file_list), 1, "No json file created")
+
+        st, end, mx = c.get_current_slice_offsets()
+        assertEqual(st, 0, "start offset should be zero")
+        assertEqual(end, 1, "end offset should be 1")
+        assertEqual(mx, TestChunkDataWriter.chunk_size - 1,\
+            "max should be {TestChunkDataWriter.chunk_size - 1}")
+
+
 
 
 if "__main__" == __name__:
